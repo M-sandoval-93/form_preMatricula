41d73e0ec632691aa462854f6e481d0c9bf23aa6
M-sandoval-93 <sandoval.mario.08@gmail.com>
2024-07-25 17:30:47 -0400

    initial project

---
+++ src/component/FormPreMatricula.jsx
@@ -0,0 +1,11 @@
+const FormPreMatricula = () => {
+
+    return (
+        <div>
+            <h1 className="text-green-500">Formulario</h1>
+        </div>
+    )
+
+}
+
+export default FormPreMatricula;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import FormPreMatricula from './component/FormPreMatricula'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FormPreMatricula />
+  </React.StrictMode>,
+)
